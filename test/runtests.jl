1fd9a00dfdd93dbe75b9692e9167138e96c6345d
Filip Tronarp <filip.tronarp@uni-tuebingen.de>
2023-01-16 17:11:49 +0100

    faster rgram_cholf

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,11 +11,12 @@ using Test
         dt = 0.5
 
         A = kron([1.0 0.0; dt 1.0], 1.0I(d))
-        L = kron([sqrt(dt) 0.0; sqrt(dt^3)/2 sqrt(dt^3/12.0)], 1.0I(d))
+        L = kron([sqrt(dt) 0.0; sqrt(dt^3)/2 sqrt(dt^3 / 12.0)], 1.0I(d))
 
         @test ndiff(M) == 1
-        @test state2diff_matrix_reverse(M, 0) == kron( hcat(zeros(1,1), fill(1.0,1,1)), 1.0I(d))
-        @test all(transition_parameters_cholf_reverse(M, dt) .== (A, L))
+        @test state2diff_matrix_reverse(M, 0) ==
+              kron(hcat(zeros(1, 1), fill(1.0, 1, 1)), 1.0I(d))
+        @test all(transition_parameters_cholf_reverse(M, dt) .≈ (A, L))
     end
 
 end
